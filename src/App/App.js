6d98cceddac5d4dafee98ce0e07f1bb55710dff6
Alex.Eggers <alex.eggers@xailabs.de>
2017-02-03 14:51:13 +0100

    Moved app into its own folder

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import './main.css';
-import StreamHeaderContainer from './StreamHeaderContainer';
-import StreamDisplay from './StreamDisplay';
-import FilterBar from './FilterBar';
+import '../main.css';
+import StreamHeaderContainer from '../StreamHeaderContainer';
+import StreamDisplay from '../StreamDisplay';
+import FilterBar from '../FilterBar';
 
 class App extends Component {
   render() {
